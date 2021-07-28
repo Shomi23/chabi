3868fc007039bc231dba7644e178027c544b9853
Shomi23 <milos.jankovic2302@gmail.com>
2021-07-28 12:30:56 +0200

    Updates

---
+++ precache-manifest.7ffbad53be699b74b7d450b79adb3443.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6edbb53b846b3df61b84647b58ebf4dc",
+    "url": "/chabi/index.html"
+  },
+  {
+    "revision": "e1c95136d25e0a26f31a",
+    "url": "/chabi/static/css/main.a3c127d5.chunk.css"
+  },
+  {
+    "revision": "c82b1a25f2ba606be75b",
+    "url": "/chabi/static/js/2.c69dc419.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chabi/static/js/2.c69dc419.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1c95136d25e0a26f31a",
+    "url": "/chabi/static/js/main.8f017525.chunk.js"
+  },
+  {
+    "revision": "35eeec86ac69f3c34263",
+    "url": "/chabi/static/js/runtime-main.2b8ac27f.js"
+  },
+  {
+    "revision": "e8a8498bec6e6e29f031fae874a504fb",
+    "url": "/chabi/static/media/correct.e8a8498b.svg"
+  },
+  {
+    "revision": "7ee757d58baeb722bbdc387fafa21c18",
+    "url": "/chabi/static/media/road.7ee757d5.svg"
+  },
+  {
+    "revision": "b5bd8a3c9cebcac27b18274124842e2a",
+    "url": "/chabi/static/media/truck-icon.b5bd8a3c.svg"
+  },
+  {
+    "revision": "4181f6d11d6d44ed34fa203e2f86965a",
+    "url": "/chabi/static/media/truck5.4181f6d1.jpeg"
+  },
+  {
+    "revision": "921d4f5c3c51e90fc752d3e089cadb58",
+    "url": "/chabi/static/media/trucks.921d4f5c.jpeg"
+  }
+]);
